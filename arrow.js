3cfaf4c1668073b1e7c10aa3effb06e30a5b11cf
unknown <giedrebielske@gmail.com>
2023-02-17 20:01:07 +0200

    Added load more button, updated design

--- arrow.js
+++ arrow.js
@@ -1,7 +1,42 @@
-var arrowUpLink = document.getElementById("arrow-up-link");
+let arrowUpLink = document.getElementById("arrow-up-link");
 
-arrowUpLink.addEventListener("click", function(event) {
+arrowUpLink.addEventListener("click", function (event) {
   event.preventDefault();
-  var target = document.getElementById("top");
+  let target = document.getElementById("top");
   target.scrollIntoView({ behavior: "smooth" });
+});
+
+let about = document.getElementsByClassName("about-btn");
+for (var i = 0, length = about.length; i < length; i++) {
+  about[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    let target = document.getElementById("top");
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+}
+
+let projects = document.getElementsByClassName("projects-btn");
+for (var i = 0, length = projects.length; i < length; i++) {
+  projects[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    let target = document.getElementById("projects");
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+}
+
+let contact = document.getElementsByClassName("contact-btn");
+for (var i = 0, length = contact.length; i < length; i++) {
+  contact[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    let target = document.getElementById("contact");
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+}
+
+const button = document.getElementById('load-more-button');
+button.addEventListener('click', function() {
+  const moreProjects = document.getElementById('loadMore');
+  moreProjects.style.display = "block";
+  moreProjects.scrollIntoView({ behavior: "smooth" });
+  button.style.display = "none"
 });
--- darkLightMode.js
+++ darkLightMode.js
@@ -4,15 +4,19 @@ document.getElementById("year").innerHTML = `${year}`;
 function switchMode() {
     let moon = document.getElementById("moon");
     let sectionColor = document.getElementsByClassName("section-color");
-    let links = document.querySelectorAll("a:not(.navigation)");
-    let arrow = document.getElementById("arrow-up-link")
+    let links = document.querySelectorAll("a");
+    let arrow = document.getElementById("arrow-up-link");
     let arrowIcon = document.getElementsByClassName("fa-arrow-up");
+    let moreButton = document.getElementById("load-more-button");
+    let linkedIn = document.getElementsByClassName("fa-linkedin");
 
     if (moon.className === "moon") {
         moon.className = "sun";
         document.body.style.backgroundColor = "#494949";
         document.body.style.color = "#fffdf6";
         arrow.style.backgroundColor = "#ece8d9";
+        moreButton.style.backgroundColor = "#ece8d9";
+        moreButton.style.color = "#494949";
 
         for (var i = 0, length = sectionColor.length; i < length; i++) {
             sectionColor[i].style.backgroundColor = "#5d5d5d";
@@ -27,11 +31,17 @@ function switchMode() {
             links[i].style.transition = "1.2s ease-in";
             links[i].style.color = "#fffdf6";
         }
+        for (var i = 0, length = linkedIn.length; i < length; i++) {
+            linkedIn[i].style.color = "#fffdf6";
+        }
     } else {
         moon.className = "moon";
         document.body.style.backgroundColor = "#ece8d9";
         document.body.style.color = "#494949";
         arrow.style.backgroundColor = "#5d5d5d";
+        moreButton.style.backgroundColor = "#5d5d5d";
+        moreButton.style.color = "#ece8d9";
+
         for (var i = 0, length = sectionColor.length; i < length; i++) {
             sectionColor[i].style.backgroundColor = "#fffdf6";
             sectionColor[i].style.color = "#494949";
@@ -44,18 +54,8 @@ function switchMode() {
         for (var i = 0, length = links.length; i < length; i++) {
             links[i].style.color = "#494949";
         }
+        for (var i = 0, length = linkedIn.length; i < length; i++) {
+            linkedIn[i].style.color = "#494949";
+        }
     }
-}
-
-function rgbToHex(rgb) {
-    var match = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    if (!match) {
-        return "";
-    }
-
-    function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
-    }
-
-    return "#" + hex(match[1]) + hex(match[2]) + hex(match[3]);
 }
